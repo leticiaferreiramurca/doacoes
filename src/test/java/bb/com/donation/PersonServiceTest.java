d84538eaa1d6f8a3b6b758688a391b1f8ac6e9e7
Igor <igor-rh@hotmail.com>
2022-08-05 13:59:31 -0300

    Tests for PersonService.java

---
+++ src/test/java/bb/com/donation/PersonServiceTest.java
@@ -0,0 +1,51 @@
+package bb.com.donation;
+
+import bb.com.donation.model.Person;
+import bb.com.donation.repository.PersonRepository;
+import bb.com.donation.service.impl.Service;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class PersonServiceTest {
+
+	@Autowired
+	private PersonRepository personRepository;
+
+	@Autowired
+	Service personService ;
+
+	@Test
+	void getPersonsTest() {
+
+		Person newPerson = new Person( 1L,"Igor");
+		personRepository.save(newPerson);
+		assertThat(personService.getById(1L).getName()).isEqualTo("Igor");
+	}
+
+	@Test
+	void savePersonTest() {
+		Person newPerson = new Person( 1L,"Igor");
+		personService.save(newPerson);
+		assertThat(personService.getById(1L).getName()).isEqualTo("Igor");
+	}
+
+
+	@Test
+	void getById() {
+		Person newPerson = new Person( 1L,"Igor");
+		personRepository.save(newPerson);
+		assertThat(personService.getById(1L).getName()).isEqualTo("Igor");
+	}
+
+	@Test
+	void getAllPersons() {
+		Person newPerson = new Person( 1L,"Igor");
+		personRepository.save(newPerson);
+		assertThat(personService.getAllPersons()).hasSize(1);
+
+	}
+}
