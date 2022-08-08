cd315b725ee3083e10da733f460041faee161e95
Igor <igor-rh@hotmail.com>
2022-08-08 17:02:08 -0300

    ProductService implementation

---
+++ src/main/java/bb/com/donation/service/impl/ProductServiceImp.java
@@ -0,0 +1,42 @@
+package bb.com.donation.service.impl;
+
+import bb.com.donation.model.Product;
+import bb.com.donation.repository.ProductRepository;
+import bb.com.donation.service.ProductService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class ProductServiceImp implements ProductService {
+
+    ProductRepository productRepository;
+
+    public ProductServiceImp(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Product getById(Long id) {
+        return productRepository.findById (id).orElse (null);
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return productRepository.findAll ();
+    }
+
+    @Override
+    public void delete(Long id) {
+        productRepository.deleteById (id);
+    }
+
+    @Override
+    public Product update(Product product) {
+        return productRepository.save (product);
+    }
+}
