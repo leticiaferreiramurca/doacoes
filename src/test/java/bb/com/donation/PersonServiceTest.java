ef884ec235444fd317be4553ac7a6951a8ad4662
Igor <igor-rh@hotmail.com>
2022-08-09 13:06:40 -0300

    Refactoring

--- src/test/java/bb/com/donation/PersonServiceTest.java
+++ src/test/java/bb/com/donation/PersonServiceTest.java
@@ -5,10 +5,6 @@ import bb.com.donation.model.Person;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
-
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
